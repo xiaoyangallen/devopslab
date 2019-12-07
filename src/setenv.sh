5a5fd1020cfb47232b12064cda89cd1440ab9a55
allen <allen@intewolf.com>
2019-12-07 14:37:33 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=allen@intewolf.com
 
 export MYCLUSTER=tektonknativebeijing44
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing44/kube-config-tok04-tektonknativebeijing44.yml
 
 export NAMESPACE=allenlxy
 export EMAIL=allen@intewolf.com
