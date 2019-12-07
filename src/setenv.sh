cc74d8706df96272931a5f619cd80caaaf866b48
allen <allen@intewolf.com>
2019-12-07 13:47:03 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
-export GITACCOUNT=
+export GITACCOUNT=allen@intewolf.com
 
 export MYCLUSTER=
 export KUBECONFIG=
 
 export NAMESPACE=
-export EMAIL=
+export EMAIL=allen@intewolf.com
 export INGRESS=
